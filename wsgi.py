b4021468fb5f391ca742e3eb6a339bd59be4969c
root <root@helmajain.rtp.raleigh.ibm.com>
2019-04-11 21:11:31 -0400

    Updating requirements

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
-app = Flask(__name__)
- 
-@app.route("/hello")
+application = Flask(__name__)
+
+@application.route("/hello")
 def hello():
     return "Hello World!"
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html")
- 
+
 if __name__ == "__main__":
-    app.run()
+    application.run()
